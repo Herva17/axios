6d8a0f1ec4d7ed2df5bc9fa8968aadd957b249ef
thierry-xo <thierrynirere@gmail.com>
2024-12-09 15:45:26 +0100

    mis à jour

---
+++ src/stores/userStore.js
@@ -0,0 +1,27 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useUserStore = defineStore("userStore", {
+  state: () => ({
+    users: [], // Liste des utilisateurs
+  }),
+  getters: {
+    state: () => {
+      getuser: (state) => state.users;
+    },
+  },
+  actions: {
+    async fetchUsers() {
+      // Réinitialiser les erreurs
+
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        this.users = response.data; // Enregistrer les utilisateurs
+      } catch (err) {
+        this.error = "Impossible de récupérer les utilisateurs."; // Capturer l'erreur
+      }
+    },
+  },
+});
